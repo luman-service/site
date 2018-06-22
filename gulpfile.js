71a0da0e9d96deef06753a941864dc5f954ee113
Aleksey Petrov <hydrognomik@gmail.com>
2018-06-22 14:32:37 +0300

    Optimization level

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@ const gulp = require('gulp')
 const imagemin = require('gulp-imagemin')
 
 gulp.task('default', () => {
-    gulp.src('src/**/*.png')
-        .pipe(imagemin({ optimizationLevel: 5 }))
-        .pipe(gulp.dest(file => {
-            return file.base
-        }))
+  gulp.src('src/**/*.png')
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 6}),
+    ]))
+    .pipe(gulp.dest(file => {
+      return file.base
+    }))
 })
